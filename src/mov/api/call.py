e6fed5b748d958e1f922c60d718a52ccc65ae89c
NishNovae <raecrowned@gmail.com>
2024-07-31 17:18:34 +0900

    update

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -20,19 +20,17 @@ def apply_type2df(load_dt='20120101', path=PARQ_PATH):
     return df
 
 def save2df(load_dt='20120101', url_param={}):
-    df = list2df(load_dt)
-    # add load_dt column YYYYMMDD w/ today's date
+    df = list2df(load_dt=load_dt, url_param=url_param)
     df['load_dt'] = load_dt
-    # partition based on load_dt
     df.to_parquet(PARQ_PATH, partition_cols=['load_dt'])
     return df
 
 def list2df(load_dt='20120101', url_param={}):
-    l = req2list(load_dt)
+    l = req2list(load_dt, url_param)
     df = pd.DataFrame(l)
     return df
 
-def req2list(load_dt='20120101') -> list:
+def req2list(load_dt='20120101', url_param={}) -> list:
     _, data = req(load_dt, option=None)
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     #print(l)
@@ -45,16 +43,14 @@ def get_key():
     key = os.getenv("MOVIE_API_KEY")
     return key
 
-def req(dt='20120101', option=None):
+def req(dt='20120101', url_param={}):
     url = gen_url(dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
-
-    #print(data)
     return code, data
 
-def gen_url(dt="20120101", req_val={"multiMovieYN": "N"}):
+def gen_url(dt="20120101", url_param={}):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}" 
@@ -65,3 +61,6 @@ def gen_url(dt="20120101", req_val={"multiMovieYN": "N"}):
         url += f"&{key}={value}"
 
     return url
+
+
+
